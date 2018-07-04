2ec97a174dcd223cb0e42ccc39f576b37b5b3f92
Brandon Lehmann <brandonlehmann@gmail.com>
2018-07-04 01:08:37 -0400

    Rebasing back to https://github.com/zone117x/node-multi-hashing as it has better multi-version Node support without the use of NAN. Patched additional issues on Windows builds that were preventing it from building and ACTUALLY running Implemented Cryptonight-lite as it's own set of include(s) and provided the naming of such.

--- tests/cryptonight-tests.js
+++ tests/cryptonight-tests.js
@@ -1,24 +1,25 @@
-const multiHashing = require('../build/Release/multihashing');
-const assert = require('assert');
+const Buffer = require('safe-buffer').Buffer
+const multiHashing = require('../build/Release/multihashing')
+const assert = require('assert')
 
-var cn_data = new Buffer("6465206f6d6e69627573206475626974616e64756d", "hex");
-var xmrig_data = new Buffer("0100fb8e8ac805899323371bb790db19218afd8db8e3755d8b90f39b3d5506a9abce4fa912244500000000ee8146d49fa93ee724deb57d12cbc6c6f3b924d946127c7a97418f9348828f0f02", "hex");
-var cn_hash = new Buffer("2f8e3df40bd11f9ac90c743ca8e32bb391da4fb98612aa3b6cdc639ee00b31f5", "hex");
-var xmrig_cnvariant1_hash = new Buffer("c9fae8425d8688dc236bcdbc42fdb42d376c6ec190501aa84b04a4b4cf1ee122", "hex");
-var xmrig_cnlite_hash = new Buffer("28a22bad3f93d1408fca472eb5ad1cbe75f21d053c8ce5b3af105a57713e21dd", "hex");
-var xmrig_cnlitevariant1_hash = new Buffer("87c4e570653eb4c2b42b7a0d546559452dfab573b82ec52f152b7ff98e79446f", "hex");
+var cndata = new Buffer('6465206f6d6e69627573206475626974616e64756d', 'hex')
+var xmrigdata = new Buffer('0100fb8e8ac805899323371bb790db19218afd8db8e3755d8b90f39b3d5506a9abce4fa912244500000000ee8146d49fa93ee724deb57d12cbc6c6f3b924d946127c7a97418f9348828f0f02', 'hex')
+var cnhash = new Buffer('2f8e3df40bd11f9ac90c743ca8e32bb391da4fb98612aa3b6cdc639ee00b31f5', 'hex')
+var xmrigcnvariant1hash = new Buffer('c9fae8425d8688dc236bcdbc42fdb42d376c6ec190501aa84b04a4b4cf1ee122', 'hex')
+var xmrigcnlitehash = new Buffer('28a22bad3f93d1408fca472eb5ad1cbe75f21d053c8ce5b3af105a57713e21dd', 'hex')
+var xmrigcnlitevariant1hash = new Buffer('87c4e570653eb4c2b42b7a0d546559452dfab573b82ec52f152b7ff98e79446f', 'hex')
 
-hashedData = multiHashing['cryptonight'](cn_data);
-cn_variant1Data = multiHashing['cryptonight'](xmrig_data, 1);
-cnlite_data = multiHashing['cryptonight-lite'](xmrig_data, 0);
-cnlite_variant1Data = multiHashing['cryptonight-lite'](xmrig_data, 1);
+var hashedData = multiHashing['cryptonight'](cndata)
+var cnvariant1Data = multiHashing['cryptonight'](xmrigdata, 1)
+var cnlitedata = multiHashing['cryptonight-lite'](xmrigdata, 0)
+var cnlitevariant1Data = multiHashing['cryptonight-lite'](xmrigdata, 1)
 
-console.log(hashedData);
-console.log(cn_variant1Data);
-console.log(cnlite_data);
-console.log(cnlite_variant1Data);
+console.log(hashedData)
+console.log(cnvariant1Data)
+console.log(cnlitedata)
+console.log(cnlitevariant1Data)
 
-assert.deepEqual(hashedData, cn_hash);
-assert.deepEqual(cn_variant1Data, xmrig_cnvariant1_hash);
-assert.deepEqual(cnlite_data, xmrig_cnlite_hash);
-assert.deepEqual(cnlite_variant1Data, xmrig_cnlitevariant1_hash);
+assert.deepEqual(hashedData, cnhash)
+assert.deepEqual(cnvariant1Data, xmrigcnvariant1hash)
+assert.deepEqual(cnlitedata, xmrigcnlitehash)
+assert.deepEqual(cnlitevariant1Data, xmrigcnlitevariant1hash)
