db1341b630686121c59942a881a7eb7734ef35e4
Brandon Lehmann <brandonlehmann@gmail.com>
2018-10-14 20:44:34 -0400

    Cryptonight variant 2 support + tests (#64)
    Reference code: https://github.com/monero-project/monero/pull/4218
    
    (cherry picked from commit f8d6b6bd9d1997ac5742648f5c29a4483c94159d)
    
    Cryptonight variant 2 - final version (#66)
    
    Reference code: https://github.com/monero-project/monero/pull/4404
    
    (cherry picked from commit c1307502b29d8b6576317924cd07a166ec79453d)
    
    Added additional tests

--- tests/cryptonight-tests.js
+++ tests/cryptonight-tests.js
@@ -5,10 +5,13 @@ const assert = require('assert')
 var xmrigdata = new Buffer('0100fb8e8ac805899323371bb790db19218afd8db8e3755d8b90f39b3d5506a9abce4fa912244500000000ee8146d49fa93ee724deb57d12cbc6c6f3b924d946127c7a97418f9348828f0f02', 'hex')
 
 var cnfasthash = new Buffer('b542df5b6e7f5f05275c98e7345884e2ac726aeeb07e03e44e0389eb86cd05f0', 'hex')
-var xmrigcnhash = new Buffer('1b606a3f4a07d6489a1bcd07697bd16696b61c8ae982f61a90160f4e52828a7f', 'hex')
+var xmrigcnvariant0hash = new Buffer('1b606a3f4a07d6489a1bcd07697bd16696b61c8ae982f61a90160f4e52828a7f', 'hex')
 var xmrigcnvariant1hash = new Buffer('c9fae8425d8688dc236bcdbc42fdb42d376c6ec190501aa84b04a4b4cf1ee122', 'hex')
-var xmrigcnlitehash = new Buffer('28a22bad3f93d1408fca472eb5ad1cbe75f21d053c8ce5b3af105a57713e21dd', 'hex')
+var xmrigcnvariant2hash = new Buffer('871fcd6823f6a879bb3f33951c8e8e891d4043880b02dfa1bb3be498b50e7578', 'hex')
+
+var xmrigcnlitevariant0hash = new Buffer('28a22bad3f93d1408fca472eb5ad1cbe75f21d053c8ce5b3af105a57713e21dd', 'hex')
 var xmrigcnlitevariant1hash = new Buffer('87c4e570653eb4c2b42b7a0d546559452dfab573b82ec52f152b7ff98e79446f', 'hex')
+var xmrigcnlitevariant2hash = new Buffer('b7e78fab22eb19cb8c9c3afe034fb53390321511bab6ab4915cd538a630c3c62', 'hex')
 
 var cnsoftshellHashv0 = []
 cnsoftshellHashv0.push(new Buffer('546c3f1badd7c1232c7a3b88cdb013f7f611b7bd3d1d2463540fccbd12997982', 'hex'))
@@ -49,10 +52,12 @@ cnsoftshellHashv1.push(new Buffer('702c3eb285452dbfce53c17da2c0d309769194880e8c3
 cnsoftshellHashv1.push(new Buffer('29e7831780a0ab930e0fe3b965f30e8a44d9b3f9ad2241d67cfbfea3ed62a64e', 'hex'))
 
 var fastHashData = multiHashing['cryptonight'](xmrigdata, true)
-var hashedDatav0 = multiHashing['cryptonight'](xmrigdata)
+var cnvariant0Data = multiHashing['cryptonight'](xmrigdata)
 var cnvariant1Data = multiHashing['cryptonight'](xmrigdata, 1)
-var cnlitedata = multiHashing['cryptonight-lite'](xmrigdata, 0)
+var cnvariant2Data = multiHashing['cryptonight'](xmrigdata, 2)
+var cnlitevariant0Data = multiHashing['cryptonight-lite'](xmrigdata, 0)
 var cnlitevariant1Data = multiHashing['cryptonight-lite'](xmrigdata, 1)
+var cnlitevariant2Data = multiHashing['cryptonight-lite'](xmrigdata, 2)
 
 // Easy fill soft shell data
 var cnsoftshellDatav0 = []
@@ -66,15 +71,23 @@ for (i = 0; i <= 8192; i += 512) {
   cnsoftshellDatav1.push({height: i, hash: multiHashing['cryptonight-soft-shell'](xmrigdata, 1, i)})
 }
 
+// Easy fill soft shell data
+var cnsoftshellDatav2 = []
+for (i = 0; i <= 8192; i += 512) {
+  cnsoftshellDatav2.push({height: i, hash: multiHashing['cryptonight-soft-shell'](xmrigdata, 2, i)})
+}
+
 console.log('')
 console.log('[#1] Cryptonight Fast Hash: ', fastHashData.toString('hex'))
-console.log('[#2] Cryptonight v0: ', hashedDatav0.toString('hex'))
+console.log('[#2] Cryptonight v0: ', cnvariant0Data.toString('hex'))
 console.log('[#3] Cryptonight v1: ', cnvariant1Data.toString('hex'))
-console.log('[#4] Cryptonight Lite v0: ', cnlitedata.toString('hex'))
-console.log('[#5] Cryptonight Lite v1: ', cnlitevariant1Data.toString('hex'))
+console.log('[#4] Cryptonight v2: ', cnvariant2Data.toString('hex'))
+console.log('[#5] Cryptonight Lite v0: ', cnlitevariant0Data.toString('hex'))
+console.log('[#6] Cryptonight Lite v1: ', cnlitevariant1Data.toString('hex'))
+console.log('[#7] Cryptonight Lite v2: ', cnlitevariant2Data.toString('hex'))
 
 // Spit out soft shell hashes
-var count = 6
+var count = 8
 console.log('')
 
 for (i = 0; i < cnsoftshellDatav0.length; i++) {
@@ -88,11 +101,20 @@ for (i = 0; i < cnsoftshellDatav1.length; i++) {
   count++
 }
 
+console.log('')
+for (i = 0; i < cnsoftshellDatav2.length; i++) {
+  console.log('[#' + count + '] Cryptonight Soft Shell v1 (' + cnsoftshellDatav2[i].height + '): ', cnsoftshellDatav2[i].hash.toString('hex'))
+  count++
+}
+
 assert.deepEqual(fastHashData, cnfasthash)
-assert.deepEqual(hashedDatav0, xmrigcnhash)
+assert.deepEqual(cnvariant0Data, xmrigcnvariant0hash)
 assert.deepEqual(cnvariant1Data, xmrigcnvariant1hash)
-assert.deepEqual(cnlitedata, xmrigcnlitehash)
+assert.deepEqual(cnvariant2Data, xmrigcnvariant2hash)
+
+assert.deepEqual(cnlitevariant0Data, xmrigcnlitevariant0hash)
 assert.deepEqual(cnlitevariant1Data, xmrigcnlitevariant1hash)
+assert.deepEqual(cnlitevariant2Data, xmrigcnlitevariant2hash)
 
 /*
 for (i = 0; i < cnsoftshellDatav0.length; i++) {
